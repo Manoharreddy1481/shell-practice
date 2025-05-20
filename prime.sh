2da085bca22183232a7407ce9fdcc0c4059e00b5
Manohar1481 <pullagurlamanoharreddy@gmail.com>
2025-05-21 00:30:35 +0530

    added files

--- prime.sh
+++ prime.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 for num in {2..100}
 do 
-    for (i=2; i<num; i++)
+    for ((i=2; i<num; i++))
     do
-        if (num%i==0)
+        if ((num%i==0))
         then
             continue 2
         fi
     done
 echo $num
 done
+
+
+# #!/bin/bash
+
+# echo "Prime numbers between 1 and 100 are:"
+
+# for num in {2..100}
+# do
+#     for (( i=2; i<num; i++ ))
+#     do
+#         if (( num % i == 0 )); then
+#             continue 2
+#         fi
+#     done
+#     echo "$num"
+# done
