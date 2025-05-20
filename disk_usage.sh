472b9c0f7ac851cfc5c9c92fd468a3e62c9aecf4
Manohar1481 <pullagurlamanoharreddy@gmail.com>
2025-05-21 00:00:42 +0530

    added files

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DISK_USAGE=$( df -ht | grep xfs )
+DISK_USAGE=$(df -ht | grep xfs)
 Disk_THRESHOLD=5
-while IFS = read -r line
+while IFS = read -r line;
 do 
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTITION_NAME=$(echo $line | grep xfs | awk -F " " '{print $NF}'
+    PARTITION_NAME=$(echo $line | grep xfs | awk -F " " '{print $NF}')
     IF [ $USAGE -gt $Disk_THRESHOLD ]
     then
         echo "$PARTITION_NAME is greater then $Disk_THRESHOLD, current value is $USAGE, Please check"
