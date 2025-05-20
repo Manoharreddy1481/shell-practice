b5f48258a7de5b36073878f677bfebbcd521b166
Manohar1481 <pullagurlamanoharreddy@gmail.com>
2025-05-21 00:05:03 +0530

    added files

--- disk_usage.sh
+++ disk_usage.sh
@@ -2,7 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 Disk_THRESHOLD=5
-while IFS = read -r line
+while IFS= read -r line
 do 
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION_NAME=$(echo $line | grep xfs | awk -F " " '{print $NF}')
