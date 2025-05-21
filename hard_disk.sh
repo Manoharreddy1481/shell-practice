b18e11737a69987434005c1402ae6c01a258cfa7
Manohar1481 <pullagurlamanoharreddy@gmail.com>
2025-05-21 10:41:12 +0530

    added files

---
+++ hard_disk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+disk_usage=$(df -hT | grep xfs)
+disk_treshold=5
+while IFS= read -r line
+do  
+    usage=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    partition_name=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [ $usage -ge $disk_treshold ]
+    then
+        echo "$partition is used more than the $disk_threshold, so, current usage is $usage, please check"
+
+    fi
+done <<<$disk_usage
