43bc3467f3dc886112f78c8acbda3659edc301a3
Manohar1481 <pullagurlamanoharreddy@gmail.com>
2025-05-21 08:52:29 +0530

    added files

--- deleting_logs_14_days.sh
+++ deleting_logs_14_days.sh
@@ -2,15 +2,15 @@
 source_dir=/home/ec2-user/logs
 if [ -d $source_dir ]
 then
-    echo " $source_dir exist"
+    echo "$source_dir exist"
 else
-    echo " $source_dir does not exist"
+    echo "$source_dir does not exist"
     exit 1
 fi
 files=$(find $source_dir -name "*.log" -mtime +14)
-echo " files are : $files"
+echo "files are : $files"
 while IFS= read -r file
 do
-    echo "deleting file: $file"
+    echo "deleting the file: $file"
     rm -rf $file
 done <<<$files
