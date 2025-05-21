274f84b924625dc2223947ef4b490ab7f42e7d1d
Manohar1481 <pullagurlamanoharreddy@gmail.com>
2025-05-21 10:46:58 +0530

    added files

---
+++ logs_14days.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+folder=/home/ec2-user/logs
+if [ -d $folder ]
+then
+    echo "$folder is exist"
+else
+    echo "$folder is does not exist"
+    exit 1
+fi
+files=$(find $folder -name "*.log -mtime +14)
+    echo "files are : $files"
+    while IFS= read -r file
+    do
+        echo "deleting the file : $file"
+        rm -rf $file
+    done <<<$files
+        
+
