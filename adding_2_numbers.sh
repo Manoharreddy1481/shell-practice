a99305d31483ba888873c1a3547e61ea7c860db7
Manohar1481 <pullagurlamanoharreddy@gmail.com>
2025-05-21 08:19:43 +0530

    added files

--- adding_2_numbers.sh
+++ adding_2_numbers.sh
@@ -3,3 +3,9 @@ NUMBER1=$1
 NUMBER2=$2
 sum=$(($NUMBER1+$NUMBER2))
 echo "sum is: $sum"
+echo " All variables: $@"
+echo " count of variables: $#"
+echo " script Nmae: $0"
+echo "process instance id: $$"
+echo "PID of the last background command: $!"
+echo " state of a previos command: $?"
